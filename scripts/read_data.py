f1e6475da5da974b4978c433cbe1678f61208a10
Batuhan Taskaya <isidentical@gmail.com>
2021-12-28 23:09:54 +0300

    Get rid of scripts/

--- scripts/read_data.py
+++
@@ -1,9 +0,0 @@
-import gzip
-import json
-import sys
-from pathlib import Path
-
-with gzip.open(Path(sys.argv[1]), "rt") as stream:
-    data = json.load(stream)
-
-print(json.dumps(data, indent=4, ensure_ascii=False))
